1c475e9f1cb2ed7ab03a50f1d086dfa8f12223db
haohaoxiansen <lin17746076220@163.com>
2017-07-18 21:36:06 +0800

    回答问题

--- router/problem.js
+++ router/problem.js
@@ -41,7 +41,55 @@ router.post("/api/saveproblem",function(req,res){
 })
 router.get("/answer/:id",function(req,res){
     console.log(req.params.id);
-    Problem.find("")
+    Problem.findById(req.params.id,function(err,data){
+        if (err) {
+            res.json({
+                code:"error",
+                msg:"数据库错误"
+            });
+        } else {
+            var problem = data.toObject()
+            problem.id = data._id;
+            delete data._id;
+            res.render("answer",{
+                problem:problem
+            })
+        }
+    })
+})
+router.post("/saveanswer",function(req,res){
+    var answertilte = req.body.title;
+    var answer = {}
+    answer.account = req.cookies.user.account;
+    answer.title = answertilte;
+    answer.time = util.getCurTime();
+    answer.ip = util.changeIp(req.ip);
+
+    Problem.findById(req.body.problemid,function(err,data){
+        if (err) {
+            res.json({
+                code:"error",
+                msg:"数据库错误"
+            });
+        } else {
+            var problem = data.toObject()
+            problem.answers.push(answer);
+
+            problem.update(function(err){
+                if(err){
+                    res.json({
+                        code:"error",
+                        msg:"回答问题失败"
+                    })
+                }else{
+                    res.json({
+                        code:"success",
+                        msg:"回答问题成功"
+                    })
+                }
+            })
+        }
+    })
 })
 
 module.exports = router;
