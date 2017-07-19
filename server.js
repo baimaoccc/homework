c5a81259aea3c2b5ea16d992a3a985dd317b4d4c
baimao <1099382073@qq.com>
2017-07-19 11:15:50 +0800

    未修改

--- server.js
+++ server.js
@@ -12,17 +12,11 @@ app.use(cookieParser());
 app.engine('html',template.__express );
 app.set('view engine', 'html');
 
-
-
-
 app.use(require('./router/login'));
 app.use(require('./router/register'));
 app.use(require('./router/header'));
 app.use(require("./router/question"));
 app.use(require("./router/modify"));
-
-// app.use(require('./router/login').router);
-
 
 app.listen(8080,function () {
     console.log('server on 8080...');
