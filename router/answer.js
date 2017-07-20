8843ac8ae210dca175d8e92d957bac720466cf7a
haohaoxiansen <lin17746076220@163.com>
2017-07-20 10:16:43 +0800

    消除了一些重定义的方法

--- router/answer.js
+++ router/answer.js
@@ -1,57 +1,115 @@
 var exp = require('express');
 var router = exp.Router();
-var MyUser = require('../db').MyUser;
-var Problem = require('../db').Problem;
 var util = require('../util');
 
+var Answer = require("../db").Answer;
+var Problem = require('../db').Problem;
 //对form表单的逻辑处理
 router.post("/saveAnswer", function (req, res) {
     var answertilte = req.body.title;
-    var answer = {};
-
-    //从浏览器回去回答的信息，包括时间等
-    answer.id = req.cookies.user._id;
+    var answer = {}
+    answer.user = req.cookies.user._id;
     answer.title = answertilte;
     answer.time = util.getCurTime();
     answer.ip = util.changeIp(req.ip);
+    answer.problem = req.body.problemid;
 
-
-    Problem.findById(req.body.problemid, function (err, data) {
+    var entity = new Answer(answer);
+    //保存回答记录
+    entity.save(function (err, data) {
         if (err) {
             res.json({
                 code: "error",
                 msg: "数据库错误"
-            });
+            })
         } else {
+            //将回答记录保存后获取回答记录的id
+            answerId = data._id;
+            //用问题的id 找出该问题
+            Problem.findOne({_id: req.body.problemid})
+                .populate('problem')
+                .exec(function (err, data) {
+                    if (err) {
+                        res.json({
+                            code: "error",
+                            msg: "数据库错误"
+                        })
+                    } else {
+                        //将查询的结果中的answers中追加回答，只要存回答的id，可以使用populate方法将id转成对象
+                        data.answers.push(answerId);
 
-            var problem = data;
-            var arr = JSON.parse(problem.answers);
-            arr.push(answer);
-            problem.answers = JSON.stringify(arr);
-            Problem.findByIdAndUpdate(req.body.problemid, problem, function (err) {
-                if (err) {
-                    console.log(err);
-                    res.json({
-                        code: "error",
-                        msg: "回答问题失败"
-                    })
-                } else {
-                    res.json({
-                        code: "success",
-                        msg: "回答问题成功"
-                    })
-                }
-            });
-
+                        //因为保存一个回答后，问题也应该有追加的回答，所以需要在数据库中修改一下当前这个问题
+                        Problem.update({_id: data._id}, data, function (err) {
+                            if (err) {
+                                res.json({
+                                    code: "error",
+                                    msg: "回答问题失败"
+                                })
+                            } else {
+                                res.json({
+                                    code: "success",
+                                    msg: "回答问题成功"
+                                })
+                            }
+                        });
+                    }
+                })
         }
     })
-});
+
+
+    // Problem.findById(req.body.problemid, function (err, data) {
+    //     if (err) {
+    //         res.json({
+    //             code: "error",
+    //             msg: "数据库错误"
+    //         });
+    //     } else {
+    //         var problem = data.toObject()
+    //         var obj = JSON.parse(problem.answers);
+    //         obj.push(answer);
+    //         problem.answers = JSON.stringify(obj);
+    //         Problem.findByIdAndUpdate(req.body.problemid, problem, function (err) {
+    //             if (err) {
+    //                 console.log(err);
+    //                 res.json({
+    //                     code: "error",
+    //                     msg: "回答问题失败"
+    //                 })
+    //             } else {
+    //                 res.json({
+    //                     code: "success",
+    //                     msg: "回答问题成功"
+    //                 })
+    //             }
+    //         })
+    //
+    //     }
+    // })
+})
+
 //该路由的处理逻辑由首页点击回答问题后触发
 router.get("/answer/:id", function (req, res) {
     var id = req.params.id;
-    Problem.findById(req.params.id)
-
-
+    Problem.findById(id)
+        .populate('createuser',{img:1,account:1,_id:0})
+        .exec(function(err,data){
+            var problem = data;
+            problem.createuser.img = "../"+problem.createuser.img;
+            if(err){
+                console.log(err);
+                res.json({
+                    code:"error",
+                    msg:"数据库错误"
+                })
+            }else{
+                res.render("answer",{
+                    problem:problem,
+                    id:req.params.id,
+                    title:"回答"
+                })
+            }
+        })
     // var allUser = [];
     // MyUser.find({},function(err,users){
     //     allUser = users;
@@ -91,5 +149,6 @@ router.get("/answer/:id", function (req, res) {
     //     }
     // })
 });
+
 
 module.exports = router;
--- router/question.js
+++ router/question.js
@@ -4,8 +4,7 @@
 var util = require("../util");
 const express = require("express");
 var Problem = require("../db").Problem;
-var MyUser = require("../db").MyUser;
-var Answer = require("../db").Answer;
+
 
 
 var router = express.Router();
@@ -18,9 +17,6 @@ var router = express.Router();
  createuser:Object,
  answers:[Object]
  */
-
-
-
 //问题页面的提交处理 将用户的问题保存在数据库中
 router.post("/api/saveproblem", function (req, res) {
     var title = req.body.title;
@@ -49,109 +45,5 @@ router.post("/api/saveproblem", function (req, res) {
         }
     });
 });
-router.get("/answer/:id", function (req, res) {
-    Problem.findOne({_id: req.params.id})
-        .populate('createuser')
-        .exec(function (err, data) {
-            if (err) {
-                res.json({
-                    code: "error",
-                    msg: "数据库错误"
-                });
-            } else {
-                data.createuser.img = "../" + data.createuser.img;
-                res.render("answer", {
-                    problem: data,
-                    id: req.params.id
-                })
-            }
-        })
-
-});
-
-
-router.post("/saveanswer", function (req, res) {
-    var answertilte = req.body.title;
-    var answer = {}
-    answer.user = req.cookies.user._id;
-    answer.title = answertilte;
-    answer.time = util.getCurTime();
-    answer.ip = util.changeIp(req.ip);
-    answer.problem = req.body.problemid;
-
-    var entity = new Answer(answer);
-    //保存回答记录
-    entity.save(function (err, data) {
-        if (err) {
-            res.json({
-                code: "error",
-                msg: "数据库错误"
-            })
-        } else {
-            //将回答记录保存后获取回答记录的id
-            answerId = data._id;
-            //用问题的id 找出该问题
-            Problem.findOne({_id: req.body.problemid})
-                .populate('problem')
-                .exec(function (err, data) {
-                    if (err) {
-                        res.json({
-                            code: "error",
-                            msg: "数据库错误"
-                        })
-                    } else {
-                        //将查询的结果中的answers中追加回答，只要存回答的id，可以使用populate方法将id转成对象
-                        data.answers.push(answerId);
-
-                        //因为保存一个回答后，问题也应该有追加的回答，所以需要在数据库中修改一下当前这个问题
-                        Problem.update({_id: data._id}, data, function (err) {
-                            if (err) {
-                                res.json({
-                                    code: "error",
-                                    msg: "回答问题失败"
-                                })
-                            } else {
-                                res.json({
-                                    code: "success",
-                                    msg: "回答问题成功"
-                                })
-                            }
-                        });
-                    }
-                })
-        }
-    })
-
-
-    // Problem.findById(req.body.problemid, function (err, data) {
-    //     if (err) {
-    //         res.json({
-    //             code: "error",
-    //             msg: "数据库错误"
-    //         });
-    //     } else {
-    //         var problem = data.toObject()
-    //         var obj = JSON.parse(problem.answers);
-    //         obj.push(answer);
-    //         problem.answers = JSON.stringify(obj);
-    //         Problem.findByIdAndUpdate(req.body.problemid, problem, function (err) {
-    //             if (err) {
-    //                 console.log(err);
-    //                 res.json({
-    //                     code: "error",
-    //                     msg: "回答问题失败"
-    //                 })
-    //             } else {
-    //                 res.json({
-    //                     code: "success",
-    //                     msg: "回答问题成功"
-    //                 })
-    //             }
-    //         })
-    //
-    //     }
-    // })
-})
-
 
 module.exports = router;
