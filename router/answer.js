a756a3354722b5b817bcd4bacd211497402bad76
haohaoxiansen <lin17746076220@163.com>
2017-07-20 09:53:00 +0800

    转到dev分支下

--- router/answer.js
+++ router/answer.js
@@ -28,8 +28,6 @@ router.post("/saveAnswer", function (req, res) {
             var arr = JSON.parse(problem.answers);
             arr.push(answer);
             problem.answers = JSON.stringify(arr);
-
-
             Problem.findByIdAndUpdate(req.body.problemid, problem, function (err) {
                 if (err) {
                     console.log(err);
@@ -48,55 +46,50 @@ router.post("/saveAnswer", function (req, res) {
         }
     })
 });
-
-
-
-// 8080/answer/xxxxxxxxxxx
-// /answer/xxxxxxxxxxx
-// "/answer/" + index;
-
-
-
 //该路由的处理逻辑由首页点击回答问题后触发
 router.get("/answer/:id", function (req, res) {
-    var allUser = [];
-    MyUser.find({},function(err,users){
-        allUser = users;
-    });
-    Problem.findById(req.params.id, function (err, data) {
-        if (err) {
-            //此时回传的浏览器页面是首页
-            res.json({
-                code: "error",
-                msg: "数据库错误"
-            });
-        } else {
-
-            //将数据库中查询到的结果转为对象problem ，
-            // problem仅用于展示，不是完全的数据库内容
-
-            var problem = data.toObject();
-            for(var i=0;i<allUser.length;i++) {
-
-                //当遍历用户数组，目的使用用户信息给problem设置其余不用存在数据库中的信息
-                if (allUser[i]._id == problem.createuser) {
-                    problem.cuserimg = "../" + allUser[i].img;
-
-                    problem.cuseraccount = allUser[i].account;
-                    res.render("answer", {
-                        title:'回答问题',
-                        problem: problem,
-
-                        //每个问题的id不同
-                        id: req.params.id
-                    });
-                    break;
-                }
-            }
+    var id = req.params.id;
+    Problem.findById(req.params.id)
 
 
-        }
-    })
+    // var allUser = [];
+    // MyUser.find({},function(err,users){
+    //     allUser = users;
+    // });
+    // Problem.findById(req.params.id, function (err, data) {
+    //     if (err) {
+    //         //此时回传的浏览器页面是首页
+    //         res.json({
+    //             code: "error",
+    //             msg: "数据库错误"
+    //         });
+    //     } else {
+    //
+    //         //将数据库中查询到的结果转为对象problem ，
+    //         // problem仅用于展示，不是完全的数据库内容
+    //
+    //         var problem = data.toObject();
+    //         for(var i=0;i<allUser.length;i++) {
+    //
+    //             //当遍历用户数组，目的使用用户信息给problem设置其余不用存在数据库中的信息
+    //             if (allUser[i]._id == problem.createuser) {
+    //                 problem.cuserimg = "../" + allUser[i].img;
+    //
+    //                 problem.cuseraccount = allUser[i].account;
+    //                 res.render("answer", {
+    //                     title:'回答问题',
+    //                     problem: problem,
+    //
+    //                     //每个问题的id不同
+    //                     id: req.params.id
+    //                 });
+    //                 break;
+    //             }
+    //         }
+    //
+    //
+    //     }
+    // })
 });
 
 module.exports = router;
